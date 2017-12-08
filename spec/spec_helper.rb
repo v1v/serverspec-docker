aaf43bb985e7ebcfbbf5279e27494bd787e4ad6a
Victor Martinez <VictorMartinezRubio@gmail.com>
2017-12-08 21:42:37 +0000

    Extend sleep

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,8 +4,8 @@ require "serverspec"
 Docker.options = { read_timeout: 600, write_timeout: 600 }
 
 def sleep
-    puts 'Wait for jenkins at least 30 seconds'
-    command("sleep 30").stdout
+    puts 'Wait for jenkins at least 60 seconds'
+    command("sleep 60").stdout
 end
 
 def debugVar

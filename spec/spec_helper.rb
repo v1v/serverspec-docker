51ed95ff9c16f2ace5b7076b51293bc8aa1a0aaa
Victor Martinez <VictorMartinezRubio@gmail.com>
2017-12-08 21:31:16 +0000

    Add more sleep to see whether in the CI works as expected

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,5 +5,5 @@ Docker.options = { read_timeout: 600, write_timeout: 600 }
 
 def sleep
     puts 'Wait for jenkins at least 20 seconds'
-    command("sleep 20").stdout
+    command("sleep 30").stdout
 end
